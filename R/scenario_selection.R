202b8862b7eacf1e24457c77e98d7fb8e056f3ae
wltcwpf <wltcwpf@gmail.com>
2022-03-06 14:17:51 -0800

    add min_rate_adj

--- R/scenario_selection.R
+++ R/scenario_selection.R
@@ -24,6 +24,10 @@
 #' weights are preferred, then a vector with the same length as \code{Y} for the desired weights
 #' can be specified. Since the weight of 1/\code{Y} has been set in the function, so you'd need
 #' to multiply your desired weights by \code{Y} to compensate the default weights.
+#' @param min_rate_adj The minimum rate multiplier (or adjustments) for the selected events.
+#' The default is 1, which means that the selected events with rate adjustments less than
+#' \code{min_rate_adj} will be removed and the rate adjustments of the remaining selected
+#' events will be updated. Note \code{min_rate_adj} needs to be non-negative value.
 #'
 #' @return A list of three elements is returned. The first element is
 #' the corresponding adjusted rate multipliers for each event
@@ -46,7 +50,7 @@
 #'
 #' @export
 scenario_selection <- function(Y, X, min_hazard = 1e-7, output_dir = NULL, max_rate_multiplier = NULL,
-                               num_lambda = 10000, Weight = 1) {
+                               num_lambda = 10000, Weight = 1, min_rate_adj = 1) {
 
   Weight <- (1 / Y) * Weight
 
@@ -90,7 +94,8 @@ scenario_selection <- function(Y, X, min_hazard = 1e-7, output_dir = NULL, max_r
   }
 
   # get the series of selected event sets
-  num_increasing <- apply(lasso_res$relaxed$beta, 2, function(x) sum(x > 1)) # only keep the variables that have multiplier > 1
+  if (min_rate_adj < 0) stop('min_rate_adj is not valid, please correct it!')
+  num_increasing <- apply(lasso_res$relaxed$beta, 2, function(x) sum(x > min_rate_adj)) # only keep the variables that have multiplier > min_rate_adj
   uni_num <- sort(unique(num_increasing[num_increasing > 0]))
   idx <- as.numeric(sapply(uni_num, function(x) {
     # find the index such that the corresponding dev.ratio is max.
@@ -110,12 +115,13 @@ scenario_selection <- function(Y, X, min_hazard = 1e-7, output_dir = NULL, max_r
 
   for (i in 1:length(idx)) {
     betas <- lasso_res$relaxed$beta[, idx[i]]
-    betas <- ifelse(betas > 1, betas, 0)
+    betas <- ifelse(betas > min_rate_adj, betas, 0)
 
     # correct the penalized coefficients
     re_correction_factor <- log(Y[Idx_include]) - log(X[Idx_include, ] %*% betas)
     re_correction_factor <- exp(mean(re_correction_factor[re_correction_factor != Inf]))
-    betas <- betas * re_correction_factor
+    if (re_correction_factor > 1)
+      betas <- betas * re_correction_factor
     betas_mat[, i] <- betas
 
     m_sq_logerr[i] <- mean((log(Y[Idx_include]) -
